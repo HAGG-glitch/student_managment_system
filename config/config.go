32601b9e187312562ac0a7f543fca15b1f1732c4
HAGG-glitch <joshuayoki1@gmail.com>
2025-09-04 13:13:08 +0000

    Update DB connection to use environment variables for deployment

--- config/config.go
+++ config/config.go
@@ -3,20 +3,24 @@ package config
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectDB() (*gorm.DB, error, bool) {
-	// Load environment variables
-	host := "localhost"
-	port := "5432"
-	user := "super_user"
-	password := "123456"
-	dbname := "school_system_2"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbname, port,
+	)
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Println("Failed to connect to database:", err)
