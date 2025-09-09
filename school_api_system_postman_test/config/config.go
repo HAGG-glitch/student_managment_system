3023d9c4889831ae0d8891650a4a0a2ea327d04d
HAGG-glitch <joshuayoki1@gmail.com>
2025-09-09 09:56:10 +0000

    used the .env file valus for the database

--- school_api_system_postman_test/config/config.go
+++ school_api_system_postman_test/config/config.go
@@ -3,19 +3,38 @@ package config
 import (
 	"fmt"
 	"log"
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+type DBConfig struct {
+    Host     string
+    Port     string
+    User     string
+    Password string
+    Name     string
+}
+
 func ConnectDB() (*gorm.DB, error, bool) {
 	// Load environment variables
-	host := "localhost"
-	port := "5432"
-	user := "super_user"
-	password := "123456"
-	dbname := "school_system_2"
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+	 cfg := DBConfig{
+        Host:     os.Getenv("DB_HOST"),
+        Port:     os.Getenv("DB_PORT"),
+        User:     os.Getenv("DB_USER"),
+        Password: os.Getenv("DB_PASSWORD"),
+        Name:     os.Getenv("DB_NAME"),
+    }
+
+	 if cfg.Host == "" || cfg.Port == "" || cfg.User == "" || cfg.Password == "" || cfg.Name == "" {
+        panic("Database environment variables not fully set")
+    }
+	
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", cfg.Host, cfg.User, cfg.Password, cfg.Name, cfg.Port)
+	
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Println("Failed to connect to database:", err)
